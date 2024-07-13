92c2c1d622f799638258bb2d06d2b81bd9e80fe6
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2024-07-13 11:05:09 +0530

    dummy data inserted

--- src/app/(app)/app/main/page.jsx
+++ src/app/(app)/app/main/page.jsx
@@ -1,12 +1,18 @@
 import TopData from '@/app/components/top-data'
 import TopBar from '@/app/components/topbar'
 import React from 'react'
+import  data from '../../../../../public/data.json'
+import Folder from '@/app/components/folders'
 
 function page() {
+  const {Floors}=data;
   return (
     <>
     <TopBar/>
     <TopData/>
+    {Floors.map((_, index) => (
+        <Folder key={index} />
+      ))}
     </>
     
   )
---
+++ src/app/components/folders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Folder() {
+  return (
+    <div className='text-black'>Floor 1</div>
+  )
+}
+
+export default Folder
